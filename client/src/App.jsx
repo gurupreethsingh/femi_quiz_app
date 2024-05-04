25095f048579c3de468a2e3df897787749e9d8cf
Gurupreeth <igurupreet@gmail.com>
2024-05-05 00:04:05 +0530

    project setup done, added bootstrap, tailwind css, Hello world code in jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl">Hello world.</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
