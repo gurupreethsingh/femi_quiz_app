2f2d0ee2e16de8540d2a5e85fd96c8b12c3076a3
Gurupreeth <igurupreet@gmail.com>
2024-08-10 23:27:16 +0530

    minor changes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,10 +21,7 @@ import Register from "./pages/Register";
 import ResetPassword from "./pages/ResetPassword";
 import SingleBlog from "./pages/SingleBlog";
 import SingleCourse from "./pages/SingleCourse";
-<<<<<<< HEAD
-=======
 import SingleExam from "./pages/SingleExam";
->>>>>>> 033917d046c9641d5a34dbdb93b5edd3a65971a5
 import SingleTutorial from "./pages/SingleTutorial";
 import SingleUser from "./pages/SingleUser";
 import StudentDashboard from "./pages/StudentDashboard";
@@ -40,9 +37,7 @@ function App() {
           <Route path="/about" element={<About />}></Route>
           <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
           <Route path="/all-courses" element={<Courses />}></Route>
-<<<<<<< HEAD
           <Route path="/single-course/:id" element={<SingleCourse />}></Route>
-=======
           <Route path="/all-exams" element={<Exams />}></Route>
           <Route path="/all-users" element={<Allusers />}></Route>
           <Route path="/blogpage" element={<BlogPage />}></Route>
@@ -60,14 +55,19 @@ function App() {
           <Route path="/single-blog/:id" element={<SingleBlog />}></Route>
           <Route path="/single-course/:id" element={<SingleCourse />}></Route>
           <Route path="/single-exam/:id" element={<SingleExam />}></Route>
->>>>>>> 033917d046c9641d5a34dbdb93b5edd3a65971a5
           <Route
             path="/single-tutorial/:id"
             element={<SingleTutorial />}
           ></Route>
           <Route path="/single-user/:id" element={<SingleUser />}></Route>
-          <Route path="/student-dashboard" element={<StudentDashboard />}></Route>
-          <Route path="/teacher-dashboard" element={<TeacherDashboard />}></Route>
+          <Route
+            path="/student-dashboard"
+            element={<StudentDashboard />}
+          ></Route>
+          <Route
+            path="/teacher-dashboard"
+            element={<TeacherDashboard />}
+          ></Route>
           <Route path="/*" element={<PageNotFound />}></Route>
         </Routes>
       </Router>
